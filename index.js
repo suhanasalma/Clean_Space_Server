29df5fc5705d20add0d3038f746b52feafe6a2fa
suhanasalma <suhanasalma800@gmail.com>
2022-11-10 12:41:13 +0600

    jwt added

--- index.js
+++ index.js
@@ -21,74 +21,109 @@ const client = new MongoClient(uri, {
 });
 
 
+function verifyJWT (req,res,next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    res.status(401).send({message:'unathorized access'})
+  }
+  const token = authHeader.split(' ')[1];
+  console.log(token)
+  jwt.verify(token,process.env.JWT_Token, function(err,decoded){
+    if(err){
+      res.send(401).send({message:'unathorized user'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+  
+}
+
+
 async function run(){
    try{
-      const serviceCollection = client
-        .db("cleaningService")
-        .collection("Services");
+     const serviceCollection = client
+       .db("cleaningService")
+       .collection("Services");
 
-      const reviewCollection = client.db("cleaningService").collection('reviews')
+     const reviewCollection = client
+       .db("cleaningService")
+       .collection("reviews");
 
-        app.get('/services',async(req,res)=>{
-         const page = parseInt(req.query.page)
-         const size = parseInt(req.query.size)
-         const query = {};
-         const cursor = serviceCollection.find(query);
-         const services = await cursor.skip(page*size).limit(size).toArray();
-         const count = await serviceCollection.estimatedDocumentCount()
-         res.send({count,services});
+     app.post("/jwt", (req, res) => {
+       const user = req.body;
+       const token = jwt.sign(user, process.env.JWT_Token, { expiresIn: "1h" });
+       res.send({ token });
+     });
 
-        })
+     app.get("/services", async (req, res) => {
+       const page = parseInt(req.query.page);
+       const size = parseInt(req.query.size);
+       const query = {};
+       const cursor = serviceCollection.find(query);
+       const services = await cursor
+         .skip(page * size)
+         .limit(size)
+         .toArray();
+       const count = await serviceCollection.estimatedDocumentCount();
+       res.send({ count, services });
+     });
 
-        app.post("/services",async(req,res)=>{
-         const service = req.body;
-         const result = await serviceCollection.insertOne(service)
-         res.send(result)
+     app.post("/services", async (req, res) => {
+       const service = req.body;
+       const result = await serviceCollection.insertOne(service);
+       res.send(result);
+     });
 
-        });
+     app.get("/services/:id", async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: ObjectId(id) };
+       const service = await serviceCollection.findOne(query);
+       res.send(service);
+     });
 
-        app.get('/services/:id',async(req,res)=>{
-         const id = req.params.id
-         const query = {_id:ObjectId(id)}  
-         const service = await serviceCollection.findOne(query);
-         res.send(service)
+     app.post("/comments", async (req, res) => {
+       const comment = req.body;
+       const result = await reviewCollection.insertOne(comment);
+       res.send(result);
+     });
 
-        })
-
-
-        app.post("/comments", async (req, res) => {
-          const comment = req.body;
-          const result = await reviewCollection.insertOne(comment);
-          res.send(result);
-        });
-
-
-           app.get("/comments", async (req, res) => {
-             let query = {};
-
-            if (req.query.email) {
-               query = { email: req.query.email };
-            }
-             if (req.query.post) {
-               query = { post: req.query.post };
-             }
-             const cursor = reviewCollection.find(query);
-             const comments = await cursor.toArray();
-             res.send(comments);
-           });
-
-          app.delete("/comments/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await reviewCollection.deleteOne(query);
-            res.send(result);
-          });
-
-            
+     app.get("/allcoments", async (req, res) => {
+       let query = {};
+       if (req.query.post) {
+         query = { post: req.query.post };
+         
+       }
+       const cursor = reviewCollection.find(query);
+       const comments = await cursor.toArray();
+       res.send(comments);
+     });
 
 
+       app.get("/comments", verifyJWT, async (req, res) => {
+       const decoded = req.decoded;
+       console.log("inside order api", decoded);
+       if (decoded.email !== req.query.email) {
+         res.status(403).send({ message: "unauthorized access" });
+       }
 
-        
+       let query = {};
+
+       if (req.query.email) {
+         query = { email: req.query.email };
+       }
+       const cursor = reviewCollection.find(query);
+       const comments = await cursor.toArray();
+       res.send(comments);
+     });
+
+     
+
+     app.delete("/comments/:id", async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: ObjectId(id) };
+       const result = await reviewCollection.deleteOne(query);
+       res.send(result);
+     });
    }
    finally{
 
